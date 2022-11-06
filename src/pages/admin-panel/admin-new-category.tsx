3b4382beab060fb9aff75fc7b6c5c08f7b79dde5
Deepali <deepali@mindpathtech.com>
2022-11-06 14:55:21 +0530

    add more three screen

--- src/pages/admin-panel/admin-new-category.tsx
+++ src/pages/admin-panel/admin-new-category.tsx
@@ -3,42 +3,21 @@ import {
   Box,
   Button,
   Container,
-  FormControl,
   Grid,
-  List,
-  ListItem,
-  MenuItem,
-  Select,
   TextField,
   Typography,
-  useTheme,
 } from "@mui/material";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faPlus, faUser } from "@fortawesome/free-solid-svg-icons";
+import { AdminSidebar } from "components";
+import { theme } from "theme";
 
 export function AdminNewCategory() {
-  const theme = useTheme();
-
   return (
     <Container maxWidth="lg" sx={{ p: 4 }}>
       <Grid container>
         <Grid item xs={12} md={2}>
-          <Box>
-            <List>
-              {Array(7)
-                .fill({
-                  name: "Dashboard",
-                })
-                .map((element, index: number) => (
-                  <ListItem key={`${element.name}-${index}`}>
-                    <FontAwesomeIcon icon={faUser} />
-                    <Typography variant="body1" fontWeight="600" sx={{ ml: 1 }}>
-                      {element.name}
-                    </Typography>
-                  </ListItem>
-                ))}
-            </List>
-          </Box>
+          <AdminSidebar />
         </Grid>
         <Grid item xs={12} md={10}>
           <Container maxWidth="xs" sx={{ my: 1, ml: 0.5 }}>
@@ -47,7 +26,6 @@ export function AdminNewCategory() {
               size="lg"
               color={theme.palette.info.main}
             />
-
             <Button
               size="large"
               sx={{
@@ -57,37 +35,23 @@ export function AdminNewCategory() {
             >
               Back
             </Button>
-
-            <Box sx={{ my: 2 }}>
-              <FormControl sx={{ width: 300 }}>
+            <Box sx={{ my: 2, alignItems: "center" }}>
+              <Box sx={{ display: "flex" }}>
                 <Typography variant="body2" fontWeight={500}>
-                  Select Business Category
+                  Category name
                 </Typography>
-                <Select
-                  fullWidth
-                  variant="outlined"
-                  labelId="sort-by-select-label"
-                  id="sort-by-simple-select"
-                  size="medium"
-                  sx={{ my: 1 }}
+                <Typography
+                  variant="caption"
+                  fontWeight={400}
+                  sx={{ ml: 2, color: theme.palette.grey[400] }}
                 >
-                  <MenuItem value={"Newest"}>Newest</MenuItem>
-                  <MenuItem value={"Oldest"}>Oldest</MenuItem>
-                </Select>
-              </FormControl>
-
-              <Box sx={{ my: 2 }}>
-                <Typography variant="body2" fontWeight={500}>
-                  Subcategory name
+                  *required
                 </Typography>
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <TextField
-                    fullWidth
-                    sx={{ my: 1, borderRadius: "30px", mr: 1 }}
-                  />
-                  <FontAwesomeIcon icon={faPlus} size="lg" color="black" />
-                </Box>
               </Box>
+              <TextField
+                fullWidth
+                sx={{ my: 1, borderRadius: "30px", mr: 1 }}
+              />
             </Box>
             <Box
               sx={{
@@ -107,7 +71,7 @@ export function AdminNewCategory() {
                 }}
                 variant="rounded"
               >
-                Add Subcategory
+                Add Category
               </Button>
             </Box>
           </Container>
--- src/pages/admin-panel/admin-new-subcategory.tsx
+++ src/pages/admin-panel/admin-new-subcategory.tsx
@@ -1,10 +1,121 @@
 import React from "react";
-import { Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  List,
+  ListItem,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { AdminSidebar } from "components";
 
-export function AdminNewSubcategory() {
+export function AdminNewSubCategory() {
+  const theme = useTheme();
+
   return (
     <Container maxWidth="lg" sx={{ p: 4 }}>
-      AdminNewSubcategory
+      <Grid container>
+        <Grid item xs={12} md={2}>
+          <AdminSidebar />
+        </Grid>
+        <Grid item xs={12} md={10}>
+          <Container maxWidth="xs" sx={{ my: 1, ml: 0.5 }}>
+            <FontAwesomeIcon
+              icon={faAngleLeft}
+              size="lg"
+              color={theme.palette.info.main}
+            />
+
+            <Button
+              size="large"
+              sx={{
+                fontWeight: 800,
+                color: "black",
+              }}
+            >
+              Back
+            </Button>
+
+            <Box sx={{ my: 2 }}>
+              <FormControl sx={{ width: 300 }}>
+                <Box sx={{ display: "flex" }}>
+                  <Typography variant="body2" fontWeight={500}>
+                    Select Business Category
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    fontWeight={400}
+                    sx={{ ml: 2, color: theme.palette.grey[400] }}
+                  >
+                    *required
+                  </Typography>
+                </Box>
+                <Select
+                  fullWidth
+                  variant="outlined"
+                  labelId="sort-by-select-label"
+                  id="sort-by-simple-select"
+                  size="medium"
+                  sx={{ my: 1 }}
+                >
+                  <MenuItem value={"Newest"}>Newest</MenuItem>
+                  <MenuItem value={"Oldest"}>Oldest</MenuItem>
+                </Select>
+              </FormControl>
+              <Box sx={{ my: 2, alignItems: "center" }}>
+                <Box sx={{ display: "flex" }}>
+                  <Typography variant="body2" fontWeight={500}>
+                    Subcategory name
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    fontWeight={400}
+                    sx={{ ml: 2, color: theme.palette.grey[400] }}
+                  >
+                    *required
+                  </Typography>
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <TextField
+                    fullWidth
+                    sx={{ my: 1, borderRadius: "30px", mr: 1 }}
+                  />
+                  <FontAwesomeIcon icon={faPlus} size="lg" color="black" />
+                </Box>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                my: 2,
+                mx: "auto",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Button
+                size="large"
+                sx={{
+                  fontWeight: 800,
+                  backgroundColor: theme.palette.info.main,
+                  color: "white",
+                }}
+                variant="rounded"
+              >
+                Add Subcategory
+              </Button>
+            </Box>
+          </Container>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
