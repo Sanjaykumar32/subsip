6d5dddb3af4d61379d20f14b78c8bde19f214a96
Samyak Joshi <skylardane12@gmail.com>
2022-11-08 21:14:05 +0530

    listing page

--- src/pages/list-on-poshsub.tsx
+++ src/pages/list-on-poshsub.tsx
@@ -1,10 +1,62 @@
 import React from "react";
-import { Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormGroup,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Label } from "components";
+import Grid from "@mui/material/Grid";
+
+interface ListFormItem {
+  label: string;
+}
 
 export function ListingOnPoshSub() {
+  const textField: ListFormItem[] = [
+    { label: "What is your buisness called?" },
+    { label: "Step 2: Tag your product in that category" },
+    { label: "Tagline" },
+    { label: "Tell us about your buisness?" },
+    { label: "Support Email?" },
+    { label: "Featured Image?" },
+    { label: "Pick a Buisness Category" },
+    { label: "Pick subcategory" },
+    { label: "Buisness Location" },
+  ];
   return (
     <Container maxWidth="lg" sx={{ p: 4 }}>
-      Listing-on-poshSub
+      <Typography variant="alternet"> List on Poshsub </Typography>
+
+      <FormGroup>
+        <Grid container>
+          {textField.map((element: ListFormItem) => (
+            <Grid item xs={12} md={6}>
+              <Box sx={{ px: 3, py: 1 }}>
+                <Label> {element.label} </Label>
+                <TextField required fullWidth />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </FormGroup>
+
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          mt: 10,
+        }}
+      >
+        <Button variant="rounded"> Submit for Approval </Button>
+        <Link sx={{ mt: 4 }}> Request to remove a existing listing </Link>
+      </Box>
     </Container>
   );
 }
