899dd2eeade2ede4d30a4b2e3a61b09811a899ed
Deepali <deepali@mindpathtech.com>
2022-11-15 07:53:41 +0530

    add pages

--- src/index.tsx
+++ src/index.tsx
@@ -140,22 +140,22 @@ const router = createBrowserRouter([
         path: "/admin/categories",
         element: <AdminCategories />,
       },
-      // {
-      //   path: "/admin/dashboard",
-      //   element: <AdminDashboard />,
-      // },
-      // {
-      //   path: "/admin/listingToSubscribers",
-      //   element: <AdminListingToSubscribers />,
-      // },
+      {
+        path: "/admin/dashboard",
+        element: <AdminDashboard />,
+      },
+      {
+        path: "/admin/listingToSubscribers",
+        element: <AdminListingToSubscribers />,
+      },
       {
         path: "/admin/listing",
         element: <AdminListing />,
       },
-      // {
-      //   path: "/admin/newlisting",
-      //   element: <AdminNewlisting />,
-      // },
+      {
+        path: "/admin/newlisting",
+        element: <AdminNewlisting />,
+      },
       {
         path: "/admin/referralToSubscribe",
         element: <AdminReferralToSubscribe />,
@@ -164,10 +164,10 @@ const router = createBrowserRouter([
         path: "/admin/referral",
         element: <AdminReferral />,
       },
-      // {
-      //   path: "/admin/rewardToDetails",
-      //   element: <AdminRewardToDetails />,
-      // },
+      {
+        path: "/admin/rewardToDetails",
+        element: <AdminRewardToDetails />,
+      },
       // {
       //   path: "/admin/rewardsToWinner",
       //   element: <AdminRewardsToWinner />,
--- src/pages/admin-panel/admin-dashboard.tsx
+++ src/pages/admin-panel/admin-dashboard.tsx
@@ -1,10 +1,124 @@
 import React from "react";
-import { Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AdminSidebar } from "components";
+import { theme } from "theme";
 
 export function AdminDashboard() {
   return (
-    <Container maxWidth="lg" sx={{ p: 4 }}>
-      Admin Dashboard
+    <Container maxWidth="lg" sx={{ p: 4, my: 5 }}>
+      <Grid container>
+        <Grid item xs={12} md={2}>
+          <AdminSidebar />
+        </Grid>
+        <Grid item xs={12} md={10}>
+          <Box
+            sx={{
+              display: "flex",
+              height: "100px",
+              my: 2,
+            }}
+          >
+            <Box
+              sx={{
+                backgroundColor: theme.palette.grey[900],
+                width: "300px",
+                borderRadius: "4px",
+              }}
+            >
+              <Box sx={{ ml: 3, my: 2 }}>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "white", fontWeight: [600] }}
+                >
+                  Total listings
+                </Typography>
+                <Typography variant="h6" sx={{ color: "white", my: 1 }}>
+                  150
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.grey[900],
+                width: "300px",
+                borderRadius: "4px",
+                ml: 2,
+              }}
+            >
+              <Box sx={{ ml: 3, my: 2 }}>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "white", fontWeight: [600] }}
+                >
+                  Total subscribers
+                </Typography>
+                <Typography variant="h6" sx={{ color: "white", my: 1 }}>
+                  1,500,000
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              height: "100px",
+              my: 1,
+            }}
+          >
+            <Box
+              sx={{
+                backgroundColor: theme.palette.grey[900],
+                width: "300px",
+                borderRadius: "4px",
+              }}
+            >
+              <Box sx={{ ml: 3, my: 2 }}>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "white", fontWeight: [600] }}
+                >
+                  Unverified subscribers
+                </Typography>
+                <Typography variant="h6" sx={{ color: "white", my: 1 }}>
+                  22,577
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.grey[900],
+                width: "300px",
+                borderRadius: "4px",
+                ml: 2,
+              }}
+            >
+              <Box sx={{ ml: 3, my: 2 }}>
+                <Typography
+                  variant="body2"
+                  sx={{ color: "white", fontWeight: [600] }}
+                >
+                  Pending listings
+                </Typography>
+                <Typography variant="h6" sx={{ color: "white", my: 1 }}>
+                  77
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
