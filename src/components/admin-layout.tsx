093c7aebea1bbdcc94295b59a3d78b2f631e4347
Deepali <deepali@mindpathtech.com>
2022-12-08 16:28:38 +0530

    fix admin view

--- src/components/admin-layout.tsx
+++ src/components/admin-layout.tsx
@@ -5,17 +5,28 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Outlet } from "react-router-dom";
+import Link from "@mui/material/Link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBell,
+  faDiagramProject,
+  faHome,
+  faList,
+  faSearch,
+  faTrophy,
+  faUser,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { Badge, InputAdornment, TextField, useTheme } from "@mui/material";
 
 const drawerWidth = 240;
 
@@ -30,38 +41,69 @@ interface Props {
 export function AdminLayout(props: Props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const theme = useTheme();
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+
+  const menuList = React.useMemo(
+    () => [
+      {
+        title: "Dashboard",
+        icon: faHome,
+      },
+      {
+        title: "Listing",
+        icon: faList,
+      },
+      {
+        title: "Subscribers",
+        icon: faHome,
+      },
+      {
+        title: "Categories",
+        icon: faUser,
+      },
+      {
+        title: "Notifications",
+        icon: faBell,
+      },
+      {
+        title: "Rewards",
+        icon: faTrophy,
+      },
+      {
+        title: "Referrals",
+        icon: faDiagramProject,
+      },
+    ],
+    []
+  );
 
   const drawer = (
     <div>
       <Toolbar />
       <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
+        {menuList.map((text, index) => (
+          <Link
+            key={text.title}
+            sx={{
+              color: theme.palette.getContrastText(
+                theme.palette.background.default
+              ),
+            }}
+          >
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <FontAwesomeIcon icon={text.icon} />
+                </ListItemIcon>
+                <ListItemText primary={text.title} />
+              </ListItemButton>
+            </ListItem>
+          </Link>
         ))}
       </List>
     </div>
@@ -73,14 +115,15 @@ export function AdminLayout(props: Props) {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
+
       <AppBar
+        color="default"
         position="fixed"
         sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
+          zIndex: (theme) => theme.zIndex.drawer + 1,
         }}
       >
-        <Toolbar>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -90,9 +133,46 @@ export function AdminLayout(props: Props) {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              fontFamily: "Kessel",
+              fontWeight: 700,
+              letterSpacing: ".2rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            PoshSub
           </Typography>
+          <TextField
+            size="small"
+            sx={{ width: "50%" }}
+            label="Search Listings"
+            InputProps={{
+              sx: { borderRadius: "60px" },
+              endAdornment: (
+                <InputAdornment position="end" sx={{ mx: 1 }}>
+                  <FontAwesomeIcon icon={faSearch} />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Box>
+            <Badge badgeContent={5} color="primary" sx={{ mx: 2 }}>
+              <FontAwesomeIcon icon={faBell} size="lg" />
+            </Badge>
+            <FontAwesomeIcon
+              icon={faUserCircle}
+              size="xl"
+              style={{ margin: theme.spacing(0, 2) }}
+            />
+          </Box>
         </Toolbar>
       </AppBar>
       <Box
